003cbf6b2b57b37d64374cbb5e375a6c4134f528
Godslovelee <godslovelee@yahoo.com>
2022-01-17 02:13:46 +0100

    "added ListVIew"

--- lib/coffee_ui_display/home.dart
+++ lib/coffee_ui_display/home.dart
@@ -19,13 +19,36 @@ class _HomeScreenState extends State<HomeScreen> {
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Text("Hello User!",style: TextStyle(
-                fontSize: 30.0,
-                fontWeight: FontWeight.bold
-              ),),
-
-
+              Text(
+                "Hello User!",
+                style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
+              ),
+              Padding(
+                padding: EdgeInsets.only(right: 15.0),
+                child: Container(
+                  height: 40.0,
+                  width: 40.0,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20.0),
+                      image: DecorationImage(
+                          image: AssetImage('assets/apple.png'),
+                          fit: BoxFit.cover)),
+                ),
+              )
             ],
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          Padding(
+            padding: EdgeInsets.only(right: 40),
+            child: Text(
+              "Let\'s Buy Some Coffee?",
+              style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.w300,
+                  color: Colors.redAccent),
+            ),
           )
         ],
       ),
